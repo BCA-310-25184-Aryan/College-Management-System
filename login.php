44724b3eab3bf4a1400b340dd218e6e819313a2d
BCA-310-25184-Aryan <aryan.bcastudent23.24184@cimage.in>
2026-07-21 00:44:07 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>College Management System</title>
+</head>
+<body>
+
+<h2>Admin Login</h2>
+
+<form action="dashboard.php" method="POST">
+
+Username:<br>
+<input type="text" name="username"><br><br>
+
+Password:<br>
+<input type="password" name="password"><br><br>
+
+<input type="submit" value="Login">
+
+</form>
+
+</body>
+</html>
