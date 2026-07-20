2ae32fc7eb5c0e151e625dbc7a47550a288a4aa3
BCA-310-25184-Aryan <aryan.bcastudent23.24184@cimage.in>
2026-07-21 00:45:40 +0530

    Create dashboard.php for College Management System

---
+++ dashboard.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dashboard</title>
+</head>
+<body>
+
+<h1>College Management System</h1>
+
+<h3>Welcome Admin</h3>
+
+<ul>
+<li>Student Management</li>
+<li>Faculty Management</li>
+<li>Course Management</li>
+<li>Attendance Management</li>
+</ul>
+
+</body>
+</html>
